dd050df1cc87d24d6d9079367ff2467cdb2e0647
Arne Decant <hello@arnedecant.be>
2025-02-15 18:38:05 +0100

    init

---
+++ src/types/common.ts
@@ -0,0 +1,3 @@
+export type NyxSize = 'xs'|'sm'|'md'|'lg'|'xl'
+export type NyxTheme = 'primary'|'secondary'|'success'|'warning'|'danger'|'info'|'light'|'dark'
+export type NyxStyle = 'solid'|'outline'|'ghost'|'text'
