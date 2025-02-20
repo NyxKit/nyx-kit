840b07f82442a47a824f7e46359598468ce6902b
Arne Decant <hello@arnedecant.be>
2025-02-20 01:35:01 +0100

    Updates

--- assets/NyxForm.stories-vOBMvgW0.js
+++ assets/NyxForm.stories-vOBMvgW0.js
@@ -1,4 +1,4 @@
-import{d as r,r as a}from"./vue.esm-bundler-BOhbys1Y.js";import{_ as m,a as s}from"./NyxFormField-BXw7-jb-.js";import{_ as l}from"./NyxInput-qE92XNXR.js";import{_ as n}from"./NyxSwitch-B1nElVPT.js";import{_ as p}from"./NyxSelect--RkeIidX.js";import{c as i}from"./common-BBtRsYrk.js";import"./useTeleportPosition-CSHUcrKa.js";const b={title:"Components/NyxForm",component:m,argTypes:{}},c=e=>r({components:{NyxForm:m},setup(){return{args:e}},template:`
+import{d as r,r as a}from"./vue.esm-bundler-B2XWfG50.js";import{_ as m,a as s}from"./NyxFormField-CrACOEZC.js";import{_ as l}from"./NyxInput-rb9v4BTk.js";import{_ as n}from"./NyxSwitch-D3lZ9y5y.js";import{_ as p}from"./NyxSelect-BnjI103T.js";import{c as i}from"./common-BBtRsYrk.js";import"./useTeleportPosition-CHFxyZ6P.js";const b={title:"Components/NyxForm",component:m,argTypes:{}},c=e=>r({components:{NyxForm:m},setup(){return{args:e}},template:`
     <nyx-form v-bind="args" @click="onClick">Form</nyx-form>
   `}),f=()=>()=>r({components:{NyxForm:m,NyxFormField:s,NyxInput:l,NyxSwitch:n,NyxSelect:p},setup(){return{options:a(Object.values(i).map(e=>({label:e,value:e})))}},template:`
     <nyx-form>
