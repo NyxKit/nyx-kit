2877396eb7c20303b9606576bb4928ddc1c952b1
Arne Decant <hello@arnedecant.be>
2025-02-28 11:11:45 +0100

    export NyxCarouse, NyxSlider and NyxTabs

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ import NyxBadge from './NyxBadge/NyxBadge.vue'
 import NyxBreadcrumbs from './NyxBreadcrumbs/NyxBreadcrumbs.vue'
 import NyxButton from './NyxButton/NyxButton.vue'
 import NyxCard from './NyxCard/NyxCard.vue'
+import NyxCarousel from './NyxCarousel/NyxCarousel.vue'
 import NyxCheckbox from './NyxCheckbox/NyxCheckbox.vue'
 import NyxForm from './NyxForm/NyxForm.vue'
 import NyxFormField from './NyxForm/NyxFormField.vue'
@@ -11,10 +12,12 @@ import NyxMedia from './NyxMedia/NyxMedia.vue'
 import NyxModal from './NyxModal/NyxModal.vue'
 import NyxProgress from './NyxProgress/NyxProgress.vue'
 import NyxSelect from './NyxSelect/NyxSelect.vue'
+import NyxSlider from './NyxSlider/NyxSlider.vue'
 import NyxSpinner from './NyxSpinner/NyxSpinner.vue'
 import NyxSwitch from './NyxSwitch/NyxSwitch.vue'
 import NyxTable from './NyxTable/NyxTable.vue'
 import NyxTableCell from './NyxTable/NyxTableCell.vue'
+import NyxTabs from './NyxTabs/NyxTabs.vue'
 import NyxTextarea from './NyxTextarea/NyxTextarea.vue'
 import NyxTooltip from './NyxTooltip/NyxTooltip.vue'
 
@@ -24,6 +27,7 @@ export {
   NyxBreadcrumbs,
   NyxButton,
   NyxCard,
+  NyxCarousel,
   NyxCheckbox,
   NyxForm,
   NyxFormField,
@@ -32,10 +36,12 @@ export {
   NyxModal,
   NyxProgress,
   NyxSelect,
+  NyxSlider,
   NyxSpinner,
   NyxSwitch,
   NyxTable,
   NyxTableCell,
+  NyxTabs,
   NyxTextarea,
   NyxTooltip
 }
