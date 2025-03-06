f363a64b56e0c60a81c07e06b7880a6e85f82460
Arne Decant <hello@arnedecant.be>
2025-03-06 23:03:43 +0100

    add NyxLoader

---
+++ src/classes/NyxLoader.ts
@@ -0,0 +1,49 @@
+export default class NyxLoader {
+  private static handleDefaultValue<T>(defaultValue: T | undefined, key: string, type: string): T {
+    if (defaultValue !== undefined) return defaultValue
+    throw new Error(`Could not load data, expected ${key} to be of type ${type}`)
+  }
+
+  private static handleData(data: unknown, key: string, type: string): unknown {
+    if (!data || typeof data !== 'object') {
+      return this.handleDefaultValue(undefined, key, type)
+    }
+    return (data as Record<string, unknown>)[key]
+  }
+
+  static loadString (data: unknown, key: string, defaultValue?: string): string {
+    const value: unknown = this.handleData(data, key, 'string')
+    if (typeof value === 'string') return value
+    return this.handleDefaultValue(defaultValue, key, 'string')
+  }
+
+  static loadBoolean (data: unknown, key: string, defaultValue?: boolean): boolean {
+    const value: unknown = this.handleData(data, key, 'boolean')
+    if (typeof value === 'boolean') return value
+    return this.handleDefaultValue(defaultValue, key, 'boolean')
+  }
+
+  static loadNumber (data: unknown, key: string, defaultValue?: number): number {
+    const value: unknown = this.handleData(data, key, 'number')
+    if (typeof value === 'number') return value
+    return this.handleDefaultValue(defaultValue, key, 'number')
+  }
+
+  static loadEnum<T extends string>(data: unknown, key: string, defaultValue: T, enumValues: T[]): T {
+    const value: unknown = this.handleData(data, key, 'string')
+    if (enumValues.includes(value as T)) return value as T
+    return this.handleDefaultValue(defaultValue, key, 'enum')
+  }
+
+  static loadArray<T>(data: unknown, key: string, defaultValue: T[]): T[] {
+    const value: unknown = this.handleData(data, key, 'array')
+    if (Array.isArray(value)) return value as T[]
+    return this.handleDefaultValue(defaultValue, key, 'array')
+  }
+
+  static loadObject<T>(data: unknown, key: string, defaultValue: T): T {
+    const value: unknown = this.handleData(data, key, 'object')
+    if (typeof value === 'object' && value !== null) return value as T
+    return this.handleDefaultValue(defaultValue, key, 'object')
+  }
+}
