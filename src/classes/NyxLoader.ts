42368c1790adf51d5a0f4dd40b041ca67137ac3c
Arne Decant <hello@arnedecant.be>
2025-03-07 01:02:34 +0100

    extend NyxLoader

--- src/classes/NyxLoader.ts
+++ src/classes/NyxLoader.ts
@@ -17,7 +17,19 @@ export default class NyxLoader {
     return this.handleDefaultValue(defaultValue, key, 'string')
   }
 
+  static loadStringOrNull (data: unknown, key: string, defaultValue?: string | null): string | null {
+    const value: unknown = this.handleData(data, key, 'string')
+    if (typeof value === 'string') return value
+    return this.handleDefaultValue(defaultValue, key, 'string')
+  }
+
   static loadBoolean (data: unknown, key: string, defaultValue?: boolean): boolean {
+    const value: unknown = this.handleData(data, key, 'boolean')
+    if (typeof value === 'boolean') return value
+    return this.handleDefaultValue(defaultValue, key, 'boolean')
+  }
+
+  static loadBooleanOrNull (data: unknown, key: string, defaultValue?: boolean | null): boolean | null {
     const value: unknown = this.handleData(data, key, 'boolean')
     if (typeof value === 'boolean') return value
     return this.handleDefaultValue(defaultValue, key, 'boolean')
@@ -29,7 +41,24 @@ export default class NyxLoader {
     return this.handleDefaultValue(defaultValue, key, 'number')
   }
 
+  static loadNumberOrNull (data: unknown, key: string, defaultValue?: number | null): number | null {
+    const value: unknown = this.handleData(data, key, 'number')
+    if (typeof value === 'number') return value
+    return this.handleDefaultValue(defaultValue, key, 'number')
+  }
+
   static loadEnum<T extends string>(data: unknown, key: string, defaultValue: T, enumValues: T[]): T {
+    const value: unknown = this.handleData(data, key, 'string')
+    if (enumValues.includes(value as T)) return value as T
+    return this.handleDefaultValue(defaultValue, key, 'enum')
+  }
+
+  static loadEnumOrNull<T extends string>(
+    data: unknown,
+    key: string,
+    defaultValue: T | null,
+    enumValues: T[]
+  ): T | null {
     const value: unknown = this.handleData(data, key, 'string')
     if (enumValues.includes(value as T)) return value as T
     return this.handleDefaultValue(defaultValue, key, 'enum')
@@ -41,7 +70,19 @@ export default class NyxLoader {
     return this.handleDefaultValue(defaultValue, key, 'array')
   }
 
+  static loadArrayOrNull<T>(data: unknown, key: string, defaultValue: T[]): T[] | null {
+    const value: unknown = this.handleData(data, key, 'array')
+    if (Array.isArray(value)) return value as T[]
+    return this.handleDefaultValue(defaultValue, key, 'array')
+  }
+
   static loadObject<T>(data: unknown, key: string, defaultValue: T): T {
+    const value: unknown = this.handleData(data, key, 'object')
+    if (typeof value === 'object' && value !== null) return value as T
+    return this.handleDefaultValue(defaultValue, key, 'object')
+  }
+
+  static loadObjectOrNull<T>(data: unknown, key: string, defaultValue: T): T | null {
     const value: unknown = this.handleData(data, key, 'object')
     if (typeof value === 'object' && value !== null) return value as T
     return this.handleDefaultValue(defaultValue, key, 'object')
