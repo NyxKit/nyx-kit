2281c7caf6d1fd9b55df8ecb881894e2a56c084a
Arne Decant <hello@arnedecant.be>
2026-03-17 18:06:05 +0100

    nyx log

--- src/classes/NyxLog.ts
+++ src/classes/NyxLog.ts
@@ -1,5 +1,5 @@
 export default class NyxLog {
-  private static getFormattedMessage(prefix: any, args: string[]): string {
+  private static getFormattedMessage (prefix: any, args: string[]): string {
     let p = prefix
     let a = args
     if (typeof prefix !== 'string' || !prefix.toLowerCase().startsWith('nyx')) {
@@ -9,11 +9,11 @@ export default class NyxLog {
     return `%c${ p }%c ${ a.join(' ') }`
   }
 
-  private static getPrefixStyle(): string {
+  private static getPrefixStyle (): string {
     return 'background: #9F50F0; color: white; padding: 4px 8px; border-radius: 2px; font-weight: bold; line-height: 1.5; margin-bottom: 4px; display: inline-flex;';
   }
 
-  private static getMessageStyle(type: string): string {
+  private static getMessageStyle (type: string): string {
     switch (type) {
       case 'info':
         return 'color: #4CAF50; font-weight: normal; line-height: 1.5; padding: 4px; display: inline-flex;'
@@ -26,15 +26,15 @@ export default class NyxLog {
     }
   }
 
-  private static getArgsStrings(args: any[]): string[] {
+  private static getArgsStrings (args: any[]): string[] {
     return args.filter((arg) => typeof arg === 'string')
   }
 
-  private static getArgsOther(args: any[]): any[] {
+  private static getArgsOther (args: any[]): any[] {
     return args.filter((arg) => typeof arg !== 'string')
   }
 
-  private static getCaller(): string {
+  private static getCaller (): string {
     const stack = new Error().stack
     if (stack) {
       const stackLines = stack.split('\n')
@@ -44,7 +44,7 @@ export default class NyxLog {
     return 'unknown'
   }
 
-  public static info(prefix: any, ...args: any[]): void {
+  public static info (prefix: any, ...args: any[]): void {
     console.log.apply(console, [
       this.getFormattedMessage(prefix, this.getArgsStrings(args)),
       this.getPrefixStyle(),
@@ -53,7 +53,7 @@ export default class NyxLog {
     ])
   }
 
-  public static warn(prefix: any, ...args: any[]): void {
+  public static warn (prefix: any, ...args: any[]): void {
     console.warn.apply(console, [
       this.getFormattedMessage(prefix, this.getArgsStrings(args)),
       this.getPrefixStyle(),
@@ -62,7 +62,7 @@ export default class NyxLog {
     ])
   }
 
-  public static error(prefix: any, ...args: any[]): void {
+  public static error (prefix: any, ...args: any[]): void {
     console.error.apply(console, [
       this.getFormattedMessage(prefix, this.getArgsStrings(args)),
       this.getPrefixStyle(),
